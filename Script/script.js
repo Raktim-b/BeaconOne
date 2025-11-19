8196f976e6256c230399b3044e9e58461a084661
Raktim-b <raktimb414@gmail.com>
2025-11-20 00:45:29 +0530

    complete without animations

--- Script/script.js
+++ Script/script.js
@@ -1,20 +1,58 @@
-document.addEventListener("DOMContentLoaded", () => {
-  var swiper = new Swiper(".mySwiper", {
-    loop: true,
-    spaceBetween: 10,
-    slidesPerView: 4,
-    freeMode: true,
-    watchSlidesProgress: true,
+$(document).ready(function () {
+  $(".testimonial-img-slick").slick({
+    dots: false,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    centerMode: true,
+    centerPadding: "0",
+    asNavFor: ".testimonial-cntn-slider",
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    pauseOnHover: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
   });
-  var swiper2 = new Swiper(".mySwiper2", {
-    loop: true,
-    spaceBetween: 10,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    thumbs: {
-      swiper: swiper,
-    },
+
+  $(".testimonial-cntn-slider").slick({
+    dots: false,
+    fade: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    draggable: false,
+    centerMode: true,
+    infinite: true,
   });
+
+  $(".testimonial-img-slick .slick-center").next().addClass("sl-next");
+  $(".testimonial-img-slick .slick-center").prev().addClass("sl-prev");
+
+  $(".testimonial-img-slick").on(
+    "beforeChange",
+    function (event, slick, currentSlide, nextSlide) {
+      $(this).find(".slick-center").next().removeClass("sl-next");
+      $(this).find(".slick-center").prev().removeClass("sl-prev");
+    }
+  );
+
+  $(".testimonial-img-slick").on(
+    "afterChange",
+    function (event, slick, currentSlide, nextSlide) {
+      $(this).find(".slick-center").next().addClass("sl-next");
+      $(this).find(".slick-center").prev().addClass("sl-prev");
+    }
+  );
 });
